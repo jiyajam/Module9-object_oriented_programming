4ef81858df4b83a2de3c315cb53092211c525ebc
jiya <jiya.kanhirathanpoyil@metropolia.fi>
2024-11-10 20:37:31 +0200

    Update class.py

--- class.py
+++ class.py
@@ -4,7 +4,7 @@ class Car:
     def __init__(self, registration_number, max_speed):
         self.registration_number = registration_number
         self.max_speed = max_speed
-        self.current_speed = "0 km/hr
+        self.current_speed = "0 km/hr"
         self.travelled_distance = "0 km/hr"
 
 
